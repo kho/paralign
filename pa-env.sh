165218d79385e234f43137b329a5bc6b315c492b
Wu, Ke <wuke@cs.umd.edu>
2013-04-25 16:16:00 -0400

    Find libjvm using JAVA_HOME

---
+++ pa-env.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+BASE=`dirname "$0"`
+
+# Add hadoop jars to classpath
+for i in /usr/hadoop*/*.jar /usr/hadoop*/*/*.jar; do
+    CLASSPATH=$CLASSPATH:$i
+done
+
+export CLASSPATH
+
+# Find libjvm and set LD_LIBRARY_PATH
+LIBJVM=`find "$JAVA_HOME/" -name libjvm.so`
+test "$LIBJVM" || { echo "Cannot find libjvm.so!" 1>&2; exit 1; }
+
+LIBJVM=`dirname "$LIBJVM"`
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBJVM
+
+"$@"
